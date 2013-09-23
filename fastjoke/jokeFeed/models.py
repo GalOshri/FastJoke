8b97a61fb0bce999ee85c224409f92242fe51843
p-stav <paulstavropoulos@gmail.com>
2013-09-22 19:20:09 -0700

    initial django setup
    hi.

---
+++ fastjoke/jokeFeed/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class UserProfile(models.Model):
+	#associated with one user
+	user = models.OneToOneField(User)
+	
+	numJokesPosted = models.IntegerField()
+	favorites = models.ManyToManyField('Joke')
+	
+	def __unicode__(self):
+		return self.user.first_name
+
+		
+class Joke(models.Model):
+	owner = models.ForeignKey(User, related_name='owns')
+	text = models.CharField(max_length=1000)
+	
+	views = models.IntegerField()
+	up = models.IntegerField()
+	down = models.IntegerField()
+	
+	date = models.DateField()
+	
+	def __unicode__(self):
+		return self.text
